c09ddf2c7d6969a374f8357d284f53b7e776426c
sena <you@example.com>
2023-09-19 09:34:38 +0200

    startet the second if task

---
+++ src/at/sena/basics/ifBedingungen2.java
@@ -0,0 +1,24 @@
+package at.sena.basics;
+
+import java.util.Random;
+public class ifBedingungen2 {
+    public static void main(String[] args) {
+        // Erstelle zwei Zufallszahl zwischen 0 und 100
+        Random random = new Random();
+        int randomNumber = random.nextInt(100);
+
+        // Wenn die erste Zahl kleiner ist als die zweite UND die erste Zahl kleiner ist als 50
+        // dann gib aus "Zahl 1 ist kleiner als Zahl 2 und Mini"
+
+        // Wenn die erste Zahl kleiner ist als 30 oder die zweite Zahl kleiner ist als 30
+        // dann gib aus "Eine der beiden ist kleiner als 30"
+
+        // Wenn die erste Zahl kleiner ist als 50 UND die zweite Zahl ungleich 50 ist
+        // dann gib aus "Erste Zahl klein, zweite kein 50iger"
+
+
+
+    }
+
+}
+
