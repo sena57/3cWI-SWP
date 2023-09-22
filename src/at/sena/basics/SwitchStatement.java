52e321d260aeed88ddb49b0fb1e8d5425a562ff8
sena <you@example.com>
2023-09-22 08:34:11 +0200

    finished the if tasks and now beginning the switch statement

---
+++ src/at/sena/basics/SwitchStatement.java
@@ -0,0 +1,4 @@
+package at.sena.basics;
+
+public class SwitchStatement {
+}
--- src/at/sena/basics/ifBedingungen2.java
+++ src/at/sena/basics/ifBedingungen2.java
@@ -5,17 +5,30 @@ public class ifBedingungen2 {
     public static void main(String[] args) {
         // Erstelle zwei Zufallszahl zwischen 0 und 100
         Random random = new Random();
-        int randomNumber = random.nextInt(100);
+        int randomNumber1 = random.nextInt(100);
+        int randomNumber2 = random.nextInt(100);
+
+
 
         // Wenn die erste Zahl kleiner ist als die zweite UND die erste Zahl kleiner ist als 50
         // dann gib aus "Zahl 1 ist kleiner als Zahl 2 und Mini"
+        System.out.println(randomNumber1 + " random Number 1");
+        System.out.println(randomNumber2 + " random Number 2");
 
+        if (randomNumber1 < randomNumber2 && randomNumber1 < 50) {
+            System.out.println(randomNumber1 + " ist kleiner als die Zahl " + randomNumber2 + " und Mini");
+        }
         // Wenn die erste Zahl kleiner ist als 30 oder die zweite Zahl kleiner ist als 30
         // dann gib aus "Eine der beiden ist kleiner als 30"
+        if (randomNumber1 < 30 || randomNumber2 < 30) {
+            System.out.println("Einer der beiden Zahlen ist kleiner als 30");
+        }
 
         // Wenn die erste Zahl kleiner ist als 50 UND die zweite Zahl ungleich 50 ist
         // dann gib aus "Erste Zahl klein, zweite kein 50iger"
-
+        if (randomNumber1 < 50 && randomNumber2 != 50) {
+            System.out.println("Erste Zahl klein, zweite kein 50iger");
+        }
 
 
     }
