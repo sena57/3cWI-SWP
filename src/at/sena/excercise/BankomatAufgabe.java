23bb12bfb553075bb94bb76d911e641b2ee1cc92
sena <you@example.com>
2023-09-29 09:32:37 +0200

    added the scanner into the file

--- src/at/sena/excercise/BankomatAufgabe.java
+++ src/at/sena/excercise/BankomatAufgabe.java
@@ -1,7 +1,12 @@
 package at.sena.excercise;
 
+import java.util.Scanner;
+
 public class BankomatAufgabe {
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
 
+        int money = scanner.nextInt();
+        System.out.println(money);
     }
 }
