83c374a5392bf06b05d2ab441b861d94c22307bd
sena <you@example.com>
2023-10-02 14:46:05 +0200

    continued bakomat added switch case

--- src/at/sena/excercise/BankomatAufgabe.java
+++ src/at/sena/excercise/BankomatAufgabe.java
@@ -3,10 +3,37 @@ package at.sena.excercise;
 import java.util.Scanner;
 
 public class BankomatAufgabe {
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
 
-        int money = scanner.nextInt();
-        System.out.println(money);
+        boolean isProgrammRunning = true;
+
+        while (isProgrammRunning) {
+            System.out.println("Wählen Sie die Gewünschte Funktionen aus: ");
+            selection();
+
+            Scanner scanner = new Scanner(System.in);
+            int input = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (input) {
+                case 1:
+                    System.out.println("Geben Sie einen Betrag ein den Sie Einzahlen wollen: ");
+                    String betrag1 = scanner.nextLine();
+                    System.out.println(betrag1);
+                    break;
+
+
+            }
+
+
+            isProgrammRunning = false;
+        }
     }
+
+    private static void selection() {
+        System.out.println(1 + ". Einzahlen");
+        System.out.println(2 + ". Abheben");
+        System.out.println(3 + ". Kontostand");
+    }
+
 }
