fd818e08ed2e68b122e2c25ac503b922f0177b2d
sena57 <126655518+sena57@users.noreply.github.com>
2023-10-09 14:10:51 +0200

    finished ATM

--- src/at/sena/excercise/BankomatAufgabe.java
+++ src/at/sena/excercise/BankomatAufgabe.java
@@ -6,6 +6,7 @@ public class BankomatAufgabe {
     public static void main(String[] args) {
 
         boolean isProgrammRunning = true;
+        int balance = 0;
 
         while (isProgrammRunning) {
             System.out.println("Wählen Sie die Gewünschte Funktionen aus: ");
@@ -17,23 +18,32 @@ public class BankomatAufgabe {
 
             switch (input) {
                 case 1:
-                    System.out.println("Geben Sie einen Betrag ein den Sie Einzahlen wollen: ");
-                    String betrag1 = scanner.nextLine();
-                    System.out.println(betrag1);
+                    System.out.print("Geben Sie einen Betrag ein den Sie Einzahlen wollen: ");
+                    int betrag1 = scanner.nextInt();
+                    balance += betrag1;
                     break;
 
+                case 2:
+                    System.out.print("Geben Sie einen Betrag ein den Sie Abheben wollen: ");
+                    int betrag2 = scanner.nextInt();
+                    balance -= betrag2;
+                    break;
 
+                case 3:
+                    System.out.println("Kontostand: " + balance + "€");
+                    break;
+
+                case 4:
+                    isProgrammRunning = false;
             }
-
-
-            isProgrammRunning = false;
         }
     }
 
     private static void selection() {
-        System.out.println(1 + ". Einzahlen");
-        System.out.println(2 + ". Abheben");
-        System.out.println(3 + ". Kontostand");
+        System.out.println("1. Einzahlen");
+        System.out.println("2. Abheben");
+        System.out.println("3. Kontostand");
+        System.out.println("4. Ende");
     }
 
 }
