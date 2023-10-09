3ad70d2b2f9426906e69fb56530fd4b32803516f
sena57 <126655518+sena57@users.noreply.github.com>
2023-10-09 15:41:58 +0200

    starting dice game task

---
+++ src/at/sena/excercise/DiceGame.java
@@ -0,0 +1,15 @@
+package at.sena.excercise;
+
+import java.util.Random;
+import java.util.Scanner
+
+public class DiceGame {
+    Scanner scanner = new Scanner(System.in);
+    int input = scanner.nextInt();
+
+    boolean isGameRunning = true;
+
+    while (isGameRunning) {
+
+    }
+}
