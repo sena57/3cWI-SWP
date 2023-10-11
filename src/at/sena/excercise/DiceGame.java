06d0567eeafdba4a8ab3f9f788a8359d3ca40231
sena57 <126655518+sena57@users.noreply.github.com>
2023-10-11 09:30:38 +0200

    working on the dice game

--- src/at/sena/excercise/DiceGame.java
+++ src/at/sena/excercise/DiceGame.java
@@ -1,15 +1,58 @@
 package at.sena.excercise;
 
 import java.util.Random;
-import java.util.Scanner
+import java.util.Scanner;
 
 public class DiceGame {
-    Scanner scanner = new Scanner(System.in);
-    int input = scanner.nextInt();
 
-    boolean isGameRunning = true;
+    public static void main(String[] args) {
+        menu();
+        boolean isGameRunning = true;
 
-    while (isGameRunning) {
+        while (isGameRunning) {
+            Scanner scanner = new Scanner(System.in);
+            int input = scanner.nextInt();
 
+            int sumOfPlayer = sumOfNumbers(1);
+            int sumOfComputer = sumOfNumbers(1);
+
+            if (input == 1) {
+                System.out.println("Deine gewürfelten Zahlen zusammengezählt: " + sumOfPlayer);
+                System.out.println("Die gewürfelten Zahlen des Computers zusammengezählt: " + sumOfComputer);
+            }
+
+
+            if (sumOfPlayer > sumOfComputer) {
+                System.out.println("Du gewinnst!");
+            } else if (sumOfPlayer < sumOfComputer) {
+                System.out.println("Der Computer gewinnt");
+            } else {
+                System.out.println("Unentschieden");
+            }
+
+            System.out.println("Wollen Sie weiter spielen? Tippen Sie 1 für 'ja' und 2 für 'nein'");
+            if (input == 2) {
+                isGameRunning = false;
+            }
+        }
     }
+
+    private static void menu() {
+        System.out.println("Herzlich Willkommen zu unserem Würfelspiel!");
+        System.out.println("Wollen sie das Spiel starten? Tippen Sie 1 für 'ja' und 2 für 'nein'");
+    }
+
+    private static int sumOfNumbers(int rolls) {
+        Random random = new Random();
+
+        int result = 0;
+
+        for (int i = 1; i <= rolls; i++) {
+            int dice = random.nextInt(1, 7);
+            result += dice;
+        }
+
+        return result;
+    }
+
 }
