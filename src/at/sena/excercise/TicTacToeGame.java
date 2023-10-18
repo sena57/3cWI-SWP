843c31be0f1aa412182c1b0c588b54288f942024
sena57 <126655518+sena57@users.noreply.github.com>
2023-10-18 09:34:22 +0200

    started tictactoe

---
+++ src/at/sena/excercise/TicTacToeGame.java
@@ -0,0 +1,23 @@
+package at.sena.excercise;
+
+public class TicTacToeGame {
+    public static void main(String[] args) {
+        menu();
+        char[][] gameBoard = {{' ', ' ', ' '},
+                            {' ', ' ', ' '},
+                            {' ', ' ', ' '}};
+    }
+
+    public static void menu() {
+        System.out.println("**********************");
+        System.out.println("WELCOME TO TICTACTOE!");
+        System.out.println("**********************");
+    }
+
+    public static void printGameBoard() {
+        for (int :
+             ) {
+            
+        }
+    }
+}
