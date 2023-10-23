c06dd8f51243313900ee8d6e2c99e8e430133482
sena57 <126655518+sena57@users.noreply.github.com>
2023-10-23 14:36:46 +0200

    code is able to place one X where you want

--- src/at/sena/excercise/TicTacToeGame.java
+++ src/at/sena/excercise/TicTacToeGame.java
@@ -9,13 +9,50 @@ public class TicTacToeGame {
                 {0, 0, 0},
                 {0, 0, 0}};
 
-        printGameBoard(gameBoard);
+
         System.out.print("Wo möchten sie Ihr Zeichen hinplazieren? (1-9): ");
         Scanner scanner = new Scanner(System.in);
 
         int position = scanner.nextInt();
-        System.out.println(position);
 
+        switch (position) {
+            case 1:
+                gameBoard [0][0] = 'X';
+                break;
+
+            case 2:
+                gameBoard [0][1] = 'X';
+                break;
+
+            case 3:
+                gameBoard [0][2] = 'X';
+                break;
+
+            case 4:
+                gameBoard [1][0] = 'X';
+                break;
+
+            case 5:
+                gameBoard [1][1] = 'X';
+                break;
+
+            case 6:
+                gameBoard [1][2] = 'X';
+                break;
+
+            case 7:
+                gameBoard [2][0] = 'X';
+                break;
+
+            case 8:
+                gameBoard [2][1] = 'X';
+                break;
+
+            case 9:
+                gameBoard [2][2] = 'X';
+                break;
+        }
+        printGameBoard(gameBoard);
     }
 
     public static void menu() {
