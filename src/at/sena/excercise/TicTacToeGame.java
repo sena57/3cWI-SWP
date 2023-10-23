3c19221d50d10863d8eea82abbe43d51610b25df
sena57 <126655518+sena57@users.noreply.github.com>
2023-10-23 14:23:26 +0200

    finished printing the game board

--- src/at/sena/excercise/TicTacToeGame.java
+++ src/at/sena/excercise/TicTacToeGame.java
@@ -1,23 +1,40 @@
 package at.sena.excercise;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
     public static void main(String[] args) {
         menu();
-        char[][] gameBoard = {{' ', ' ', ' '},
-                            {' ', ' ', ' '},
-                            {' ', ' ', ' '}};
+        char[][] gameBoard = {{0, 0, 0},
+                {0, 0, 0},
+                {0, 0, 0}};
+
+        printGameBoard(gameBoard);
+        System.out.print("Wo möchten sie Ihr Zeichen hinplazieren? (1-9): ");
+        Scanner scanner = new Scanner(System.in);
+
+        int position = scanner.nextInt();
+        System.out.println(position);
+
     }
 
     public static void menu() {
         System.out.println("**********************");
         System.out.println("WELCOME TO TICTACTOE!");
         System.out.println("**********************");
+        System.out.println("Visualisierung des Spielbrettes: ");
+        System.out.println("|1|2|3|");
+        System.out.println("|4|5|6|");
+        System.out.println("|7|8|9|");
     }
 
-    public static void printGameBoard() {
-        for (int :
-             ) {
-            
+    public static void printGameBoard(char[][] gameBoard) {
+        for (char[] row : gameBoard) {
+            for (char c : row) {
+                System.out.print(" | " + c + " | ");
+            }
+            System.out.println();
         }
+
     }
 }
