fe7b81f0820ae4a1016a15fcf6344ee327047316
sena57 <126655518+sena57@users.noreply.github.com>
2023-11-20 14:20:18 +0100

    changed the code a bit but not much

--- src/at/sena/excercise/WordGuessing.java
+++ src/at/sena/excercise/WordGuessing.java
@@ -8,23 +8,37 @@ public class WordGuessing {
         String words[] = {"Metallblume", "Informatik", "Kinder", "Laptop"};
         Random random = new Random();
         String randomWord = words[random.nextInt(words.length)];
-        char[] finalWord = new char[randomWord.length()];
+        char[] chars = randomWord.toCharArray();
+
 
         int i = 0;
-        while (i < finalWord.length) {
-            finalWord[i] = '*';
+        while (i < chars.length) {
+            chars[i] = '*';
             i++;
         }
-        guessingLetters(finalWord);
+        System.out.println(chars);
+        guessingLetters(chars);
     }
 
     public static void guessingLetters(char[] finalWord) {
-        Scanner scanner = new Scanner(System.in);
-        String letter = scanner.nextLine();
+        Scanner guess = new Scanner(System.in);
 
-        String guess= "";
-        while (!guess.equals(finalWord)) {
-            System.out.println();
+        boolean finished = false;
+        while (finished == false) {
+            System.out.println("Gib einen Buchstaben ein: ");
+            String letter = guess.next();
+
+            while (letter.length()) {
+
+            }
+
+            booelean found = false;
+            for (int i = 0; i < finalWord.length; i++) {
+                if(letter.charAt(0) == finalWord[i]) {
+                    found = true;
+                }
+            }
         }
+
     }
 }
